ca4ec9cc49b7f972d2346cec9156a4edf6238379
Sangeun Han <sangeun535@gmail.com>
2021-06-29 01:00:21 +0900

    practice 01

---
+++ Exam01/src/Exam01.kt
@@ -0,0 +1,31 @@
+fun main(){
+//    var var1 : Int = 10
+//    var var2 : Float = 10.1f
+//    var var3 : Double = 10.2
+//    var var4 : Char = '안'
+//    var var5 : String = "안드로이드"
+    var _var1 = 10
+    var _var2 = 10.1f
+    var _var3 = 10.2
+    var _var4 = '안'
+    var _var5 = "안드로이드"
+    var name : String = "한상은"
+    var id : Int = 2020111415
+    var age : Int = 21
+    var dept : Char = 'S'
+//    println(var1)
+//    println(var2)
+//    println(var3)
+//    println(var4)
+//    println(var5)
+    println(_var1)
+    println(_var2)
+    println(_var3)
+    println(_var4)
+    println(_var5)
+    println(name)
+    println(id)
+    println(age)
+    println(dept)
+
+}
