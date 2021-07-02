df54cbaabde297e39e3b4ba4057f066dff04eade
Sangeun Han <sangeun535@gmail.com>
2021-07-02 12:43:56 +0900

    Day4 - 1

---
+++ app/src/main/java/com/example/startapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.startapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var button1: Button
+        button1 = findViewById(R.id.btn1)
+    }
+}
