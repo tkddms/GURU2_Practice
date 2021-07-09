b3b36e6351a7aa56a1e4d812f07b77313b60a976
Sangeun Han <sangeun535@gmail.com>
2021-07-10 04:32:07 +0900

    Week2 - BMI Calculator Project1 - End

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var resultButton: Button   // resultActivity로 이동하도록 하는 Btn
+    // 키, 몸무게 값 전달
+    lateinit var heightEditText: EditText
+    lateinit var weightEditText: EditText
+    lateinit var nameEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // setting
+        resultButton = findViewById<Button>(R.id.resultButton)
+        heightEditText = findViewById<EditText>(R.id.heightEditText)
+        weightEditText = findViewById<EditText>(R.id.weightEditText)
+        nameEditText = findViewById<EditText>(R.id.nameEditText)
+        loadData()  // 기존 값 로드
+
+        resultButton.setOnClickListener {
+            saveData(heightEditText.text.toString().toInt(), weightEditText.text.toString().toInt())
+            // intent 전환
+            var intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("name", nameEditText.text.toString())
+            intent.putExtra("height", heightEditText.text.toString())
+            intent.putExtra("weight", weightEditText.text.toString())
+            startActivity(intent)
+        }
+    }
+
+    // 데이터 저장하기
+    private fun saveData(height: Int, weight: Int){
+        var pref = this.getPreferences(0)
+        var editor = pref.edit()    // 공유 변수 file에 write
+
+        editor.putInt("KEY_HEIGHT", heightEditText.text.toString().toInt()).apply()
+        editor.putInt("KEY_WEIGHT", weightEditText.text.toString().toInt()).apply()
+        editor.putString("KEY_NAME", nameEditText.text.toString()).apply()
+    }
+
+    // 데이터 불러오기
+    private fun loadData(){
+        var pref = this.getPreferences(0)
+        var height = pref.getInt("KEY_HEIGHT", 0)
+        var weight = pref.getInt("KEY_WEIGHT", 0)
+        var name = pref.getString("KEY_NAME", "")
+        // height or weight가 0일때 -> 아래 코드 실행 안함.
+        if(height != 0 && weight != 0){
+            nameEditText.setText(name.toString())
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
     }
 }
--- BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -2,10 +2,46 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class ResultActivity : AppCompatActivity() {
+
+    lateinit var resultTextView: TextView
+    lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        resultTextView = findViewById<TextView>(R.id.textView)
+        imageView = findViewById<ImageView>(R.id.imageView)
+        // null 값이 없도록 함.
+        var height = intent.getStringExtra("height")!!.toInt()
+        var weight = intent.getStringExtra("weight")!!.toInt()
+        var name = intent.getStringExtra("name")
+        // BMI 계산
+        var bmi = weight / Math.pow(height/100.0, 2.0)
+
+        // 해당 범위에 맞춰 TextView에 text 추가
+        when{
+            bmi >= 35 -> resultTextView.text = "고도 비만"
+            bmi >= 30 -> resultTextView.text = "2단계 비만"
+            bmi >= 25 -> resultTextView.text = "1단계 비만"
+            bmi >= 23 -> resultTextView.text = "과체중"
+            bmi >= 18.5 -> resultTextView.text = "정상"
+            else -> resultTextView.text = "저체중"
+        }
+        // 해당 범위에 맞춰 ImageView의 Image 설정
+        when{
+            bmi >= 23 -> imageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            bmi > 18.5 -> imageView.setImageResource(R.drawable.ic_baseline_sentiment_satisfied_alt_24)
+            else -> imageView.setImageResource(R.drawable.ic_baseline_mood_bad_24)
+        }
+
+        Toast.makeText(this, "$name" + " : " + "$bmi", Toast.LENGTH_SHORT).show()
+        
     }
 }
