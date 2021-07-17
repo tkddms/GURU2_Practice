4e65361c4044e64d5adbdc1bed0e3b86b6824a11
Sangeun Han <sangeun535@gmail.com>
2021-07-18 00:56:58 +0900

    Week3 - StudentManagement app - Start

---
+++ StudentManagement/app/src/main/java/com/example/studentmanagement/PersonalReg.kt
@@ -0,0 +1,11 @@
+package com.example.studentmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalReg : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_reg)
+    }
+}
